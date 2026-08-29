c4d974ff1a43d893df638385519adda47b51f34c
Soham Avinash Tapdiya <f20240930@pilani.bits-pilani.ac.in>
2026-08-29 17:37:29 +0000

    lab01 task3

--- labs/lab01/task3/cla4.v
+++ labs/lab01/task3/cla4.v
@@ -34,47 +34,43 @@ module cla4(
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
 
-  // Step 1: propagate / generate
+  wire c1_temp;
+  wire c2_temp1, c2_temp2;
+  wire c3_temp1, c3_temp2, c3_temp3;
+  wire cout_temp1, cout_temp2, cout_temp3, cout_temp4;
+
   xor #(2) (p0, a[0], b[0]);
   xor #(2) (p1, a[1], b[1]);
   xor #(2) (p2, a[2], b[2]);
   xor #(2) (p3, a[3], b[3]);
+
   and #(2) (g0, a[0], b[0]);
   and #(2) (g1, a[1], b[1]);
   and #(2) (g2, a[2], b[2]);
   and #(2) (g3, a[3], b[3]);
 
-  // Step 2: direct carry equations
-  // c1 = g0 + p0.cin
-  wire w1_0;
-  and #(2) (w1_0, p0, cin);
-  or  #(2) (c1, g0, w1_0);
+  and #(2) (c1_temp, p0, cin);
+  or  #(2) (c1, g0, c1_temp);
 
-  // c2 = g1 + p1.g0 + p1.p0.cin
-  wire w2_0, w2_1;
-  and #(2) (w2_0, p1, g0);
-  and #(2) (w2_1, p1, p0, cin);
-  or  #(2) (c2, g1, w2_0, w2_1);
+  and #(2) (c2_temp1, p1, g0);
+  and #(2) (c2_temp2, p1, p0, cin);
+  or  #(2) (c2, g1, c2_temp1, c2_temp2);
 
-  // c3 = g2 + p2.g1 + p2.p1.g0 + p2.p1.p0.cin
-  wire w3_0, w3_1, w3_2;
-  and #(2) (w3_0, p2, g1);
-  and #(2) (w3_1, p2, p1, g0);
-  and #(2) (w3_2, p2, p1, p0, cin);
-  or  #(2) (c3, g2, w3_0, w3_1, w3_2);
+  and #(2) (c3_temp1, p2, g1);
+  and #(2) (c3_temp2, p2, p1, g0);
+  and #(2) (c3_temp3, p2, p1, p0, cin);
+  or  #(2) (c3, g2, c3_temp1, c3_temp2, c3_temp3);
 
-  // c4 = g3 + p3.g2 + p3.p2.g1 + p3.p2.p1.g0 + p3.p2.p1.p0.cin
-  wire w4_0, w4_1, w4_2, w4_3;
-  and #(2) (w4_0, p3, g2);
-  and #(2) (w4_1, p3, p2, g1);
-  and #(2) (w4_2, p3, p2, p1, g0);
-  and #(2) (w4_3, p3, p2, p1, p0, cin);
-  or  #(2) (cout, g3, w4_0, w4_1, w4_2, w4_3);
+  and #(2) (cout_temp1, p3, g2);
+  and #(2) (cout_temp2, p3, p2, g1);
+  and #(2) (cout_temp3, p3, p2, p1, g0);
+  and #(2) (cout_temp4, p3, p2, p1, p0, cin);
+  or  #(2) (cout, g3, cout_temp1, cout_temp2, cout_temp3, cout_temp4);
 
-  // Step 3: sum bits  (c0 = cin)
   xor #(2) (sum[0], p0, cin);
   xor #(2) (sum[1], p1, c1);
   xor #(2) (sum[2], p2, c2);
   xor #(2) (sum[3], p3, c3);
+
 
 endmodule
